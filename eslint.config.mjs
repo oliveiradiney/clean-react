bdb331af2b2316f378a3269a61cedaf85e4b3a0f
Diney oliveira <diney.oliveira@sps.ce.gov.br>
2025-02-25 16:09:03 -0300

    chore: config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,31 @@
 import globals from 'globals'
-import pluginJs from '@eslint/js'
+import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import love from 'eslint-config-love'
-import eslintConfigPrettier from 'eslint-config-prettier'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ['**/node_modules/**', 'coverage/**', 'dist/**']
+    ignores: [
+      '**/node_modules/**',
+      'coverage/**',
+      'dist/**',
+      'jest.config.ts',
+      'eslint.config.mjs',
+      'webpack.*.js',
+      '**/*.scss'
+    ]
   },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-
+  love,
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        projectService: './tsconfig.json',
+        project: './tsconfig.json',
         allowAutomaticSingleRunInference: true,
         ecmaFeatures: {
           jsx: true
@@ -27,11 +37,27 @@ export default [
       react: {
         version: 'detect'
       }
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/return-await': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/class-methods-use-this': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/prefer-destructuring': 'off',
+      'react/prop-types': 'off',
+
+      '@typescript-eslint/no-unsafe-type-assertion': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-magic-numbers': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error'
     }
   },
-  love,
-  eslintConfigPrettier,
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
   pluginReact.configs.flat.recommended
 ]
