68cf6cfe1a477656c0b89d884df2dac5f3f3149b
Diney oliveira <diney.oliveira@sps.ce.gov.br>
2025-03-24 09:53:09 -0300

    feat: ensure Login will not load spinner and error on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './form-status-styles.scss'
 import Spinner from '../spinner/spinner'
+import Context from '@/presentation/contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
   return (
     <div data-testid='error-wrap' className='errorWrap'>
-      <Spinner className='spinner' />
-      <span className='error'>Erro</span>
+      {isLoading && <Spinner className='spinner' />}
+      {errorMessage && <span className='error'>{errorMessage}</span>}
     </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login-styles.scss'
 import { Header, Input, Footer, FormStatus } from '@/presentation/components'
+import Context from '@/presentation/contexts/form/form-context'
 
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
 const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
     <div className='login'>
       <Header />
-      <form className='form'>
-        <h2>Login</h2>
-        <Input type='email' name='email' placeholder='Digite seu email' />
-        <Input type='password' name='password' placeholder='Digite sua senha' />
+      <Context.Provider value={state}>
+        <form className='form'>
+          <h2>Login</h2>
+          <Input type='email' name='email' placeholder='Digite seu email' />
+          <Input
+            type='password'
+            name='password'
+            placeholder='Digite sua senha'
+          />
 
-        <button className='submit' type='submit'>
-          Entrar
-        </button>
-        <span className='link'>Criar conta</span>
-        <FormStatus />
-      </form>
+          <button className='submit' type='submit'>
+            Entrar
+          </button>
+          <span className='link'>Criar conta</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
       <Footer />
     </div>
   )
