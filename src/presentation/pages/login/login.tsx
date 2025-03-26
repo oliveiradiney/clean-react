99375dba7bce013af665cf7875234addea57b565
Diney oliveira <diney.oliveira@sps.ce.gov.br>
2025-03-26 14:10:56 -0300

    feat: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -41,7 +41,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
 
           <button
             data-testid='submit'
-            disabled
+            disabled={!!state.emailError || !!state.passwordError}
             className='submit'
             type='submit'
           >
