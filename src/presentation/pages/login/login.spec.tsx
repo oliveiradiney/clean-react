25a5f78c460d1b34d586a4c8529a4023e417d000
Diney oliveira <diney.oliveira@sps.ce.gov.br>
2025-03-26 14:11:13 -0300

    test: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -77,10 +77,25 @@ describe('Login Component', () => {
     validationStub.errorMessage = null
     const passwordInput = sut.getByTestId('password')
     fireEvent.input(passwordInput, {
-      target: { value: faker.internet.email() }
+      target: { value: faker.internet.password() }
     })
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe('Tudo certo!')
     expect(passwordStatus.textContent).toBe('🟢')
+  })
+
+  test('Should enable submit button if form is valid', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, {
+      target: { value: faker.internet.email() }
+    })
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, {
+      target: { value: faker.internet.password() }
+    })
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(false)
   })
 })
